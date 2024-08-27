3ca4dace4d82c2e6d848f88c2fede62891e191b0
xianyubb <2149656630@qq.com>
2024-08-27 17:57:56 +0800

    Initial commit

---
+++ xmake.lua
@@ -0,0 +1,49 @@
+add_rules("mode.debug", "mode.release")
+
+add_repositories("liteldev-repo https://github.com/LiteLDev/xmake-repo.git")
+add_repositories("tesessentialdev-repo https://github.com/TSEssentialDev/xmake-repo.git")
+
+-- add_requires("levilamina x.x.x") for a specific version
+-- add_requires("levilamina develop") to use develop version
+-- please note that you should add bdslibrary yourself if using dev version
+add_requires("levilamina")
+add_requires("tsessential")
+
+if not has_config("vs_runtime") then
+    set_runtimes("MD")
+end
+
+target("my-mod") -- Change this to your mod name.
+    add_cxflags(
+        "/EHa",
+        "/utf-8",
+        "/W4",
+        "/w44265",
+        "/w44289",
+        "/w44296",
+        "/w45263",
+        "/w44738",
+        "/w45204"
+    )
+    add_defines("NOMINMAX", "UNICODE")
+    add_files("src/**.cpp")
+    add_includedirs("src")
+    add_packages("levilamina")
+    add_packages("tsessential")
+    add_shflags("/DELAYLOAD:bedrock_server.dll") -- To use symbols provided by SymbolProvider.
+    set_exceptions("none") -- To avoid conflicts with /EHa.
+    set_kind("shared")
+    set_languages("c++20")
+    set_symbols("debug")
+
+    after_build(function (target)
+        local mod_packer = import("scripts.after_build")
+
+        local mod_define = {
+            modName = target:name(),
+            modFile = path.filename(target:targetfile()),
+            modVersion = major .. "." .. minor .. "." .. patch,
+        }
+        
+        mod_packer.pack_mod(target,mod_define)
+    end)
